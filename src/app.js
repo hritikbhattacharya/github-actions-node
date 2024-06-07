90ccea82e472f4aeb36627f087d4f5737fb95e2f
hritikbhattacharya <hritik7042@gmail.com>
2024-06-07 15:40:23 +0530

    home woute added

--- src/app.js
+++ src/app.js
@@ -7,4 +7,8 @@ app.get('/testNode',(req,res)=>{
     res.status(200).send("welcome to node js")
 })
 
+app.get("/",(req,res)=>{
+    res.status(200).send("This is the home page")
+})
+
 module.exports = app    
